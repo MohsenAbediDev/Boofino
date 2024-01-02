b5b4350ab3a89ec04803b603ddc42f3de7afbd40
Mohsen <mohsen63296329@gmail.com>
2024-01-02 22:18:26 +0330

    fix BackToDashboard component in Cart.jsx file

--- src/Pages/Cart.jsx
+++ src/Pages/Cart.jsx
@@ -3,10 +3,9 @@ import ProductCart from '../Common/Components/ProductCart'
 
 export default function Cart() {
 	return (
-		<>
-			<BackToDashboard title="سبد خرید" />
-
 			<div className='w-full h-full overflow-y-auto scroll'>
+				<BackToDashboard title='سبد خرید' />
+				
 				{/* Products Purchased By The User */}
 				<div className='md:divide-y-2 divide-secondary'>
 					<ProductCart />
@@ -15,6 +14,5 @@ export default function Cart() {
 					<ProductCart />
 				</div>
 			</div>
-		</>
 	)
 }
--- src/Pages/InfoUser.jsx
+++ src/Pages/InfoUser.jsx
@@ -1,7 +1,9 @@
+import BackToDashboard from '../Common/Components/BackToDashboard'
+
 export default function InfoUser() {
-  return (
-    <div className='flex flex-col w-full'>
-      
-    </div>
-  )
+	return (
+		<div className='flex flex-col w-full'>
+			<BackToDashboard title='مشخصات کاربر' />
+		</div>
+	)
 }
