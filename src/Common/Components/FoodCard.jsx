4cf1ab0e48392434ce7a40458f8c250c9a52000e
shirpalang <shirpalang@gmail.com>
2024-01-03 22:41:22 +0330

    coding Add product to cart feature

--- src/Common/Components/FoodCard.jsx
+++ src/Common/Components/FoodCard.jsx
@@ -1,30 +1,184 @@
-export default function FoodCard() {
+import { useEffect, useRef, useState } from "react"
+
+export default function FoodCard({ id, group, title, image, price, isDiscount, oldPrice }) {
+
+	const [productsID, setProductsID] = useState([])
+	const [count, setCount] = useState(1)
+	const inputRef = useRef()
+
+	console.log(productsID);
+
+	// add product to Cart
+	const addToCart = () => {
+
+		const mainProductCart = JSON.parse(localStorage.getItem('productCart'))
+
+
+		const mainProductCartID = mainProductCart ? mainProductCart.map(product => product.id) : []
+
+		if (!mainProductCartID.includes(id)) {
+
+			if (!mainProductCart) {
+				localStorage.setItem('productCart', JSON.stringify([{ id: id, count: 1 }]))
+				setProductsID([id])
+			} else {
+				localStorage.setItem('productCart', JSON.stringify([...mainProductCart, { id: id, count: 1 }]))
+				setProductsID([...mainProductCartID, id])
+			}
+		}
+	}
+
+	const addCount = () => {
+		// get current datas
+		const mainProductCart = JSON.parse(localStorage.getItem('productCart'))
+		const mainProductIndex = mainProductCart ? mainProductCart.findIndex(product => product.id === id) : -1
+
+		// add count of product from Cart
+		if (mainProductIndex !== -1) {
+
+			mainProductCart[mainProductIndex].count = mainProductCart[mainProductIndex].count + 1
+
+			const newItem = mainProductCart[mainProductIndex]
+
+			mainProductCart.splice(mainProductIndex, 1)
+
+			const newArray = [...mainProductCart, newItem]
+
+			localStorage.setItem('productCart', JSON.stringify(newArray))
+
+			inputRef.current.value = newItem.count
+		}
+	}
+
+	const minCount = () => {
+		// get current datas
+		const mainProductCart = JSON.parse(localStorage.getItem('productCart'))
+		const mainProductIndex = mainProductCart ? mainProductCart.findIndex(product => product.id === id) : -1
+
+		if (mainProductIndex !== -1) {
+
+			mainProductCart[mainProductIndex].count = mainProductCart[mainProductIndex].count - 1
+
+			const newItem = mainProductCart[mainProductIndex]
+
+			// remove product from Cart
+			if (newItem.count === 0) {
+
+				const findID = productsID.findIndex(ID => ID === newItem.id)
+
+				setProductsID(prevIDs => {
+					return prevIDs.splice(findID, 1)
+				})
+
+				mainProductCart.splice(mainProductIndex, 1)
+
+				localStorage.setItem('productCart', JSON.stringify(mainProductCart))
+			} else {
+				// min count of product from Cart
+				mainProductCart.splice(mainProductIndex, 1)
+
+				const newArray = [...mainProductCart, newItem]
+
+				localStorage.setItem('productCart', JSON.stringify(newArray))
+
+				inputRef.current.value = newItem.count
+			}
+
+		}
+	}
+
+	useEffect(() => {
+		const mainProductCart = JSON.parse(localStorage.getItem('productCart'))
+
+		const mainProductCartID = mainProductCart ? mainProductCart.map(product => product.id) : []
+
+		setProductsID(mainProductCartID)
+
+		setCount(prevCount => {
+			const mainProductCart = JSON.parse(localStorage.getItem('productCart'))
+			const mainProductIndex = mainProductCart ? mainProductCart.findIndex(product => product.id === id) : -1
+
+			if (mainProductIndex !== -1) {
+				return mainProductCart[mainProductIndex].count
+			} else {
+				return prevCount
+			}
+		})
+	}, [])
+
 	return (
 		<>
 			<div
-				className='bg-white h-72 min-w-[180px] rounded-md p-3 relative mx-3
-			md:min-w-[175px] md:h-[270px] select-none' 
+				className='bg-white h-72 max-w-[180px] min-w-[180px] rounded-md p-3 relative mx-3
+				md:min-w-[175px] md:max-w-[175px] md:h-[270px] select-none'
 			>
-				<div className='w-full h-full relative  cp'>
-					<div className='h-[150px] w-full mx-auto bg-cover bg-[url(./images/3.jpg)] rounded-md'></div>
+				<div className='w-full h-full relative'>
+					<div className={`h-[150px] w-full mx-auto bg-cover bg-[url(${image})] rounded-md`}></div>
 					<h1
-						className='text-black mt-1 text-2xl font-extrabold font-shabnam
-					md:text-xl md:mt-0.5'
+						className='text-black mt-1 text-lg font-extrabold font-shabnam
+					md:text-md md:mt-0.5'
 					>
-						ساندویچ سرد
+						{title}
 					</h1>
-					<h3
-						className='text-price mt-2 text-[16px] font-extrabold font-shabnam
-					md:mt-1'
-					>
-						۱۹,۰۰۰ تومان
-					</h3>
-					<button
-						className='bg-primaryBTN hover:bg-hoverBTN transition-colors w-full h-8 font-shabnam text-white rounded-md absolute bottom-0
-					md:h-8'
-					>
-						افزودن به سبد خرید
-					</button>
+
+					{/* price */}
+					{
+						!isDiscount ? (
+							<h3
+								className='text-price mt-1 text-sm font-extrabold font-shabnam
+								md:mt-1'
+							>
+								{price} تومان
+							</h3>
+
+						) : (
+							<div className="flex flex-col leading-5
+							md:flex-row md:gap-x-2">
+								<h3
+									className='text-price mt-0.5 text-sm font-extrabold font-shabnam
+									md:mt-1'
+								>
+									{price} تومان
+								</h3>
+								<h3
+									className='text-dashboardItemActive text-sm font-extrabold font-shabnam line-through decoration-red
+									md:mt-1 md:text-xs'
+								>
+									{oldPrice} تومان
+								</h3>
+							</div>
+						)
+					}
+
+					{
+						!productsID.includes(id) ? (
+							<button
+								className='bg-primaryBTN hover:bg-hoverBTN transition-colors w-full h-8 font-shabnam text-white rounded-md absolute bottom-0'
+								onClick={addToCart}
+							>
+								افزودن به سبد خرید
+							</button>
+						) : (
+							<div className="w-full h-8 font-shabnam text-white absolute bottom-0 flex justify-center gap-x-2">
+								<button className="bg-primaryBTN hover:bg-hoverBTN w-1/4 rounded-md"
+									onClick={addCount}>+</button>
+								
+								{/* count of product */}
+								<input type="text"
+
+									className="h-full w-1/3 text-black text-xl border-primaryBTN border-2 outline-primaryBTN outline-2 rounded-md
+									text-center"
+									value={count}
+									disabled
+									ref={inputRef}
+
+								/>
+
+								<button className="bg-primaryBTN hover:bg-hoverBTN w-1/4 rounded-md"
+									onClick={minCount}>-</button>
+							</div>
+						)
+					}
 				</div>
 			</div>
 		</>
--- src/Common/Components/Menus.jsx
+++ src/Common/Components/Menus.jsx
@@ -2,35 +2,69 @@ import Menu from './Menu'
 import MenuTitles from './MenuTitles'
 import FoodCard from './FoodCard'
 // import useGetFetch from '../Hooks/useGetFetch'
+import products from '../../../datas'
+import { useEffect, useRef, useState } from 'react'
 
 export default function Menus() {
 	// const {datas, isLoading, error} = useGetFetch('https://kmmwkyppcirzjhgcemof.supabase.co')
 
+	const [datas, setDatas] = useState(products)
+	const [groups, setGroups] = useState([])
+
+	// Filter groups
+	useEffect(() => {
+		const allGroups = datas.map(data => data.group)
+		const filteredGroups = new Set(allGroups)
+
+		setGroups(Array.from(filteredGroups))
+	}, [])
+
+
 	return (
 		<div className='container flex flex-col items-center justify-center mt-5'>
 			{/* Food's Menu */}
+
 			<MenuTitles title='غذاها' />
-
+			
 			<div className='w-full flex flex-col items-center'>
-				{/* Hot Food Slider */}
-				<Menu grouping='hotFood'>
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-				</Menu>
+				{
+					// show group menu
+					groups.map(group => (
+						<Menu grouping={group} key={group}>
+							{/* Show products in their own group */}
+							{
 
-				{/* Cold Food Slider */}
-				<Menu grouping='coldFood'>
+								datas.map(data => data.group === group && <FoodCard {...data} key={data.id} />)
+							}
+						</Menu>
+					))
+				}
+			</div>
+
+			{/* <MenuTitles title='غذاها' />
+
+			<div className='w-full flex flex-col items-center'> */}
+			{/* Hot Food Slider */}
+
+
+			{/* <Menu grouping='hotFood'>
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+				</Menu> */}
+
+			{/* Cold Food Slider */}
+			{/* <Menu grouping='coldFood'>
 					<FoodCard />
 					<FoodCard />
 					<FoodCard />
@@ -45,14 +79,31 @@ export default function Menus() {
 					<FoodCard />
 					<FoodCard />
 				</Menu>
-			</div>
+			</div> */}
 
 			{/* Drink's Menu */}
-			<MenuTitles title='نوشیدنی‌ها' />
+			{/* <MenuTitles title='نوشیدنی‌ها' />
 
-			<div className='w-full flex flex-col items-center'>
-				{/* Hot Drink Slider */}
-				<Menu grouping='hotDrink'>
+			<div className='w-full flex flex-col items-center'> */}
+			{/* Hot Drink Slider */}
+			{/* <Menu grouping='hotDrink'>
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+					<FoodCard />
+				</Menu> */}
+
+			{/* Cold Drink Slider */}
+			{/* <Menu grouping='coldDrink'>
 					<FoodCard />
 					<FoodCard />
 					<FoodCard />
@@ -67,27 +118,10 @@ export default function Menus() {
 					<FoodCard />
 					<FoodCard />
 				</Menu>
-
-				{/* Cold Drink Slider */}
-				<Menu grouping='coldDrink'>
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-					<FoodCard />
-				</Menu>
-			</div>
+			</div> */}
 
 			{/* Edible's Menu */}
-			<MenuTitles title='خوراکی‌ها' />
+			{/* <MenuTitles title='خوراکی‌ها' />
 
 			<div className='w-full flex flex-col items-center'>
 				<Menu grouping='edible'>
@@ -105,7 +139,7 @@ export default function Menus() {
 					<FoodCard />
 					<FoodCard />
 				</Menu>
-			</div>
+			</div> */}
 		</div>
 	)
 }
