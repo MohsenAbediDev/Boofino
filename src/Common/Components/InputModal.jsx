de74ae121ac1aaab21dec8ac1c13ff6779593093
Mohsen <mohsen63296329@gmail.com>
2024-04-26 00:43:45 +0330

    Responsive EditUser page

--- src/Common/Components/InputModal.jsx
+++ src/Common/Components/InputModal.jsx
@@ -1,7 +1,7 @@
 export default function InputModal({ children }) {
   return (
     <>
-      <div className="z-50 bg-secondary rounded-dashboardcontainer p-10 w-[600px] h-96 fixed ml-auto mr-auto left-0 right-0 mt-auto mb-auto top-0 bottom-0">
+      <div className="z-50 bg-secondary rounded-dashboardcontainer p-10 w-[600px] sm:w-[95%] h-96 fixed ml-auto mr-auto left-0 right-0 mt-auto mb-auto top-0 bottom-0">
         {children}
       </div>
     </>
