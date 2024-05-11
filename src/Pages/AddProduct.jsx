4a5164cbc57690a70abcc39468a7fc6685cab804
Mohsen <mohsen63296329@gmail.com>
2024-05-11 13:53:33 +0330

    Add notification in AddProduct page

--- src/Pages/AddProduct.jsx
+++ src/Pages/AddProduct.jsx
@@ -1,4 +1,5 @@
 import { useState, useEffect } from 'react'
+import Notification from '../Common/Components/Notification'
 
 export default function AddProduct() {
 	const groups = [
@@ -22,6 +23,17 @@ export default function AddProduct() {
 	const [off, setOff] = useState('')
 	const [count, setCount] = useState('')
 
+	//? Notification Variable's
+	const [successMessage, setSuccessMessage] = useState('')
+	const [errorMessage, setErrorMessage] = useState('')
+	const [isShowNotification, setIsShowNotification] = useState(false)
+
+	const showingNotification = () => {
+		setIsShowNotification(true)
+
+		setTimeout(() => setIsShowNotification(false), 3000)
+	}
+
 	// send picture
 	const sendPictureHandler = (e) => {
 		const selectedFile = e.target.files[0]
@@ -32,12 +44,10 @@ export default function AddProduct() {
 
 	// send group
 	const senGroupHandler = () => {
-    console.log({name: group});
-
 		if (!group == '') {
 			fetch('http://localhost:3000/addcategory', {
 				method: 'POST',
-				body: JSON.stringify({"name": group}),
+				body: JSON.stringify({ _name: group }),
 			})
 				.then((res) => res.json())
 				.then((data) => console.log(data))
@@ -61,7 +71,9 @@ export default function AddProduct() {
 				const data = await res.json()
 				return data
 			} catch (err) {
-				alert('مشکلی در بارگذاری عکس پیش آمده')
+				setSuccessMessage('')
+				setErrorMessage('مشکلی در بارگذاری عکس پیش آمده')
+				showingNotification()
 			}
 		}
 	}
@@ -86,152 +98,171 @@ export default function AddProduct() {
 					}),
 				})
 				const data = await res.json()
-				console.log(data)
+
+				setErrorMessage('')
+				setSuccessMessage('محصول با موفقیت افزوده شد')
+				showingNotification()
 			} catch (err) {
-				console.log(err)
+				setSuccessMessage('')
+				setErrorMessage('خطا در اضافه کردن محصول! دوباره تلاش کنید')
+				showingNotification()
 			}
 		} else {
-			alert('لطفا تمام فیلد هارو پر کنید')
+			setSuccessMessage('')
+			setErrorMessage('لطفا تمام فیلد هارا پر کنید')
+			showingNotification()
 		}
 	}
 
 	return (
-		<div className='dashboard-container h-fit py-5 flex flex-col gap-y-9 items-center justify-center'>
-			{/* Select image section */}
-			<div className='bg-dashboardItem shadow-xl w-40 h-40 rounded-dashboarditem flex justify-center items-center relative overflow-hidden'>
-				{/* Show image */}
-				{filePath && <img src={filePath} className='w-full h-full absolute' />}
+		<>
+			<div className='dashboard-container h-fit py-5 flex flex-col gap-y-9 items-center justify-center'>
+				{/* Select image section */}
+				<div className='bg-dashboardItem shadow-xl w-40 h-40 rounded-dashboarditem flex justify-center items-center relative overflow-hidden'>
+					{/* Show image */}
+					{filePath && (
+						<img src={filePath} className='w-full h-full absolute' />
+					)}
 
-				{/* Select image input */}
-				<label
-					htmlFor='image-upload'
-					className='bg-primaryBTN text-white cp rounded-md p-2'
-				>
-					انتخاب عکس
-				</label>
-				<input
-					type='file'
-					id='image-upload'
-					className='flex items-center mx-2 absolute right-[-999px]'
-					accept='image/*'
-					onChange={(e) => sendPictureHandler(e)}
-				/>
-			</div>
-
-			{/* Input's section */}
-			<div className='w-4/5 grid grid-cols-2 gap-5 lg:grid-cols-1'>
-				{/* Add product name */}
-				<div className='flex flex-col gap-y-4'>
-					{/* Input title */}
-					<label htmlFor='name' className='text-xl text-white'>
-						افزودن نام
-					</label>
-
-					{/* Input */}
-					<input
-						type='text'
-						id='name'
-						placeholder='ساندویچ همبرگر'
-						className='h-14 shadow-xl px-5 text-lg rounded-dashboarditem bg-dashboardItem text-white outline-none border-none'
-						value={title}
-						onChange={(e) => setTitle(e.target.value)}
-					/>
-				</div>
-
-				{/* Add product price */}
-				<div className='flex flex-col gap-y-4'>
-					{/* Input title */}
-					<label htmlFor='name' className='text-xl text-white'>
-						افزودن قیمت (تومان)
-					</label>
-
-					{/* Input */}
-					<input
-						type='number'
-						id='name'
-						min='0'
-						placeholder='12000'
-						className='h-14 shadow-xl px-5 text-lg rounded-dashboarditem bg-dashboardItem text-white outline-none border-none'
-						value={price}
-						onChange={(e) => setPrice(e.target.value)}
-					/>
-				</div>
-
-				{/* Add product group */}
-				<div className='flex flex-col gap-y-4'>
-					{/* Input title */}
-					<label htmlFor='name' className='text-xl text-white'>
-						دسته بندی
-					</label>
-
-					{/* Input */}
-					<select
-						className='h-14 cp shadow-xl px-5 text-lgxt-xl rounded-dashboarditem bg-dashboardItem text-white outline-none border-none'
-						value={group}
-						onChange={(e) => setGroup(e.target.value)}
+					{/* Select image input */}
+					<label
+						htmlFor='image-upload'
+						className='bg-primaryBTN text-white cp rounded-md p-2'
 					>
-						<option value='' disabled>
-							انتخاب کنید
-						</option>
-						{groups.map((group) => (
-							<option value={group.title} key={group.id}>
-								{group.title}
+						انتخاب عکس
+					</label>
+					<input
+						type='file'
+						id='image-upload'
+						className='flex items-center mx-2 absolute right-[-999px]'
+						accept='image/*'
+						onChange={(e) => sendPictureHandler(e)}
+					/>
+				</div>
+
+				{/* Input's section */}
+				<div className='w-4/5 grid grid-cols-2 gap-5 lg:grid-cols-1'>
+					{/* Add product name */}
+					<div className='flex flex-col gap-y-4'>
+						{/* Input title */}
+						<label htmlFor='name' className='text-xl text-white'>
+							افزودن نام
+						</label>
+
+						{/* Input */}
+						<input
+							type='text'
+							id='name'
+							placeholder='ساندویچ همبرگر'
+							className='h-14 shadow-xl px-5 text-lg rounded-dashboarditem bg-dashboardItem text-white outline-none border-none'
+							value={title}
+							onChange={(e) => setTitle(e.target.value)}
+						/>
+					</div>
+
+					{/* Add product price */}
+					<div className='flex flex-col gap-y-4'>
+						{/* Input title */}
+						<label htmlFor='name' className='text-xl text-white'>
+							افزودن قیمت (تومان)
+						</label>
+
+						{/* Input */}
+						<input
+							type='number'
+							id='name'
+							min='0'
+							placeholder='12000'
+							className='h-14 shadow-xl px-5 text-lg rounded-dashboarditem bg-dashboardItem text-white outline-none border-none'
+							value={price}
+							onChange={(e) => setPrice(e.target.value)}
+						/>
+					</div>
+
+					{/* Add product group */}
+					<div className='flex flex-col gap-y-4'>
+						{/* Input title */}
+						<label htmlFor='name' className='text-xl text-white'>
+							دسته بندی
+						</label>
+
+						{/* Input */}
+						<select
+							className='h-14 cp shadow-xl px-5 text-lgxt-xl rounded-dashboarditem bg-dashboardItem text-white outline-none border-none'
+							value={group}
+							onChange={(e) => setGroup(e.target.value)}
+						>
+							<option value='' disabled>
+								انتخاب کنید
 							</option>
-						))}
-					</select>
+							{groups.map((group) => (
+								<option value={group.title} key={group.id}>
+									{group.title}
+								</option>
+							))}
+						</select>
+					</div>
+
+					{/* Add product off */}
+					<div className='flex flex-col gap-y-4'>
+						{/* Input title */}
+						<label htmlFor='name' className='text-xl text-white'>
+							افزودن تخفیف (عدد)
+						</label>
+
+						{/* Input */}
+						<input
+							type='number'
+							id='name'
+							min='0'
+							max='100'
+							placeholder='10٪'
+							className='h-14 shadow-xl px-5 text-lg rounded-dashboarditem bg-dashboardItem text-white outline-none border-none'
+							value={off}
+							onChange={(e) => setOff(e.target.value)}
+						/>
+					</div>
+
+					{/* Add product count */}
+					<div className='flex flex-col gap-y-4'>
+						{/* Input title */}
+						<label htmlFor='name' className='text-xl text-white'>
+							تعداد محصول
+						</label>
+
+						{/* Input */}
+						<input
+							type='number'
+							id='name'
+							min='0'
+							placeholder='25'
+							className='h-14 shadow-xl px-5 text-lg rounded-dashboarditem bg-dashboardItem text-white outline-none border-none'
+							value={count}
+							onChange={(e) => setCount(e.target.value)}
+						/>
+					</div>
 				</div>
 
-				{/* Add product off */}
-				<div className='flex flex-col gap-y-4'>
-					{/* Input title */}
-					<label htmlFor='name' className='text-xl text-white'>
-						افزودن تخفیف (عدد)
-					</label>
-
-					{/* Input */}
-					<input
-						type='number'
-						id='name'
-						min='0'
-						max='100'
-						placeholder='10٪'
-						className='h-14 shadow-xl px-5 text-lg rounded-dashboarditem bg-dashboardItem text-white outline-none border-none'
-						value={off}
-						onChange={(e) => setOff(e.target.value)}
-					/>
-				</div>
-
-				{/* Add product count */}
-				<div className='flex flex-col gap-y-4'>
-					{/* Input title */}
-					<label htmlFor='name' className='text-xl text-white'>
-						تعداد محصول
-					</label>
-
-					{/* Input */}
-					<input
-						type='number'
-						id='name'
-						min='0'
-						placeholder='25'
-						className='h-14 shadow-xl px-5 text-lg rounded-dashboarditem bg-dashboardItem text-white outline-none border-none'
-						value={count}
-						onChange={(e) => setCount(e.target.value)}
-					/>
+				<div className='flex justify-end w-4/5 gap-x-5'>
+					<button className='h-12 w-24 bg-hoverBTN rounded-lg text-xl text-black'>
+						لغو
+					</button>
+					<button
+						className='h-12 w-24 bg-primaryBTN rounded-lg text-xl text-white'
+						onClick={sendToApi}
+					>
+						افزودن
+					</button>
 				</div>
 			</div>
 
-			<div className='flex justify-end w-4/5 gap-x-5'>
-				<button className='h-12 w-24 bg-hoverBTN rounded-lg text-xl text-black'>
-					لغو
-				</button>
-				<button
-					className='h-12 w-24 bg-primaryBTN rounded-lg text-xl text-white'
-					onClick={sendToApi}
-				>
-					افزودن
-				</button>
-			</div>
-		</div>
+			{/* Show Notification */}
+			{isShowNotification && (
+				<Notification
+					errorMessage={errorMessage}
+					successMessage={successMessage}
+				/>
+			)}
+		</>
 	)
 }
