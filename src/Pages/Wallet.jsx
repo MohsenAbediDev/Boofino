1b3d6d533776e96aa6363de336c39aa3afbce1e9
Mohsen <mohsen63296329@gmail.com>
2024-10-09 21:47:51 +0330

    clean codes

--- src/Pages/Wallet.jsx
+++ src/Pages/Wallet.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from 'react'
 import { getUserWallet } from '../utils/utils'
 import { NavLink } from 'react-router-dom'
+import BackToDashboard from '../Common/Components/BackToDashboard'
 
 import { IoWallet } from 'react-icons/io5'
 import { RiEmotionSadFill } from 'react-icons/ri'
@@ -22,46 +23,48 @@ export default function Wallet() {
 		getWalletValue()
 	}, [])
 	return (
-		<>
-			<div className='text-white dashboard-container h-screen flex flex-col items-center py-6 px-5'>
-				<IoWallet className='w-20 h-20' />
+		<div className='text-white dashboard-container h-screen flex flex-col items-center '>
+			<BackToDashboard title='کیف پول' />
+
+			<div className='flex items-center justify-center flex-col'>
+				<IoWallet className='w-20 h-20 mt-2' />
 
 				<span className='text-[#CECECE] font-bold text-4xl mt-4'>
 					{walletValue}
 				</span>
 
 				<p className='mt-4 text-2xl'>تومان</p>
+			</div>
 
-				<div className='w-full flex items-center justify-between gap-5 mt-10 mx-2'>
-					<NavLink
-						to='#'
-						className='bg-[#68AC50] w-2/3 h-12 text-2xl flex items-center justify-center rounded-md'>
-						واریز
-					</NavLink>
+			<div className='w-full flex items-center justify-between gap-5 mt-10 mx-2'>
+				<NavLink
+					to='#'
+					className='bg-[#68AC50] w-2/3 h-12 text-2xl flex items-center justify-center rounded-md'>
+					واریز
+				</NavLink>
 
-					<NavLink
-						to='#'
-						className='bg-redBTN w-2/3 h-12 text-2xl flex items-center justify-center rounded-md'>
-						برداشت
-					</NavLink>
-				</div>
+				<NavLink
+					to='#'
+					className='bg-redBTN w-2/3 h-12 text-2xl flex items-center justify-center rounded-md'>
+					برداشت
+				</NavLink>
+			</div>
 
-				<div className='w-full mt-16 flex flex-col items-start justify-start'>
-					<p>تاریخ تراکنش ها</p>
+			<div className='w-full mt-16 flex flex-col items-start justify-start'>
+				<p>تاریخ تراکنش ها</p>
 
-					<div className='bg-[#202225c9] w-full h-full flex items-center justify-center mt-1'>
-						{/* If the user has no transaction */}
-						<div className='w-full flex items-center justify-center flex-col my-10'>
-							<RiEmotionSadFill className='w-24 h-24' />
+				<div className='bg-[#202225c9] w-full h-full flex items-center justify-center mt-1'>
+					{/* If the user has no transaction */}
+					<div className='w-full flex items-center justify-center flex-col my-5'>
+						<RiEmotionSadFill className='w-24 h-24' />
 
-							<p className='mt-4'>شما هنوز هیچ تراکنشی انجام نداید !</p>
-						</div>
-
-						{/* If the user have transaction */}
-						<div className='hidden w-full flex items-center justify-center flex-col my-10'></div>
+						<p className='mt-4'>شما هنوز هیچ تراکنشی انجام نداید !</p>
 					</div>
+
+					{/* If the user have transaction */}
+					<div className='hidden w-full flex items-center justify-center flex-col my-10'></div>
 				</div>
 			</div>
-		</>
+		</div>
 	)
 }
