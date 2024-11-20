ef745cea51282d7b86c59945cad1100ff24c784d
Mohsen <mohsen63296329@gmail.com>
2024-11-20 23:57:48 +0330

    Change title size BackToDashboard text

--- src/Common/Components/BackToDashboard.jsx
+++ src/Common/Components/BackToDashboard.jsx
@@ -3,7 +3,7 @@ import { NavLink } from 'react-router-dom'
 
 export default function BackToDashboard(props) {
 	return (
-		<div className='flex items-center text-white text-3xl md:text-xl py-2 w-full'>
+		<div className='flex items-center text-white text-2xl md:text-xl py-2 w-full'>
 			<NavLink to='/dashboard'>
 				<MdArrowForwardIos className='text-xl cp ml-2' />
 			</NavLink>
