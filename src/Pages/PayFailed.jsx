0bf42b746924a26314f19bf21f6559a20af88f8c
Mohsen <mohsen63296329@gmail.com>
2024-11-21 00:32:12 +0330

    Edit payment-failed page

--- src/Pages/PayFailed.jsx
+++ src/Pages/PayFailed.jsx
@@ -1,7 +1,7 @@
 import { Link } from 'react-router-dom'
+import QrCodeGenerator from '../Common/Components/QrCodeGenerator'
 
 export default function PayFailed() {
-	const orderCode = 4343
 	return (
 		<div className='dashboard-container h-fit flex flex-col items-center justify-center py-5 px-5 mt-5'>
 			<div className='w-80 sm:w-full h-[450px] font-shabnam flex flex-col justify-center items-center bg-white rounded-xl dir-rtl'>
@@ -10,16 +10,16 @@ export default function PayFailed() {
 					<span className='text-[#ED2D2D] text-2xl font-bold'>
 						پرداخت ناموفق
 					</span>
-					<p className='text-[#696969] mt-1'>برای ثبت سفارش مجددا اقدام کنید</p>
+					<p className='text-[#696969] mt-1'>برای پرداخت مجددا اقدام کنید</p>
 				</div>
 
 				{/* Show QrCode */}
 				<div className='w-60 h-60 mt-2'>
-					<img src='images/example-qrcode.png' className='w-full rounded-md' />
+					<QrCodeGenerator url={`http://localhost:5173/dashboard/cart`} />
 				</div>
 
 				{/* Show Info Btn */}
-				<Link to='/'>
+				<Link to='/dashboard/cart'>
 					<button className='w-32 bg-[#257FD2] text-white text-base py-4 mt-5 rounded-md'>
 						پرداخت
 					</button>
